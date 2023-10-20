b49b40c2fa42da9ae7c6e4c2d47d478bf790438f
sethgonzales <sethgonzales157@gmail.com>
2023-10-20 11:59:10 -0700

    complete home controller and add launchsettings file

--- PierreIdentity/Controllers/HomeController.cs
+++ PierreIdentity/Controllers/HomeController.cs
@@ -1,43 +1,27 @@
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.AspNetCore.Identity;
-// using System.Threading.Tasks;
-// using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using PierreIdentity.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace ToDoList.Controllers
-// {
-//   public class HomeController : Controller
-//   {
-//     private readonly ToDoListContext _db;
-//     private readonly UserManager<ApplicationUser> _userManager;
+namespace PierreIdentity.Controllers
+{
+  public class HomeController : Controller
+  {
+    private readonly PierreIdentityContext _db;
+    public HomeController(PierreIdentityContext db)
+    {
+      _db = db;
+    }
 
-//     public HomeController(UserManager<ApplicationUser> userManager, ToDoListContext db)
-//     {
-//       _userManager = userManager;
-//       _db = db;
-//     }
-
-//     [HttpGet("/")]
-//     public async Task<ActionResult> Index()
-//     {
-//     //these actions will only happen if there is currently a logged in user. If not, these wont happen.
-//       // Category logic
-//       Category[] cats = _db.Categories.ToArray();
-//       Dictionary<string, object[]> model = new Dictionary<string, object[]>(); //dictionaty will hold our category and item information
-//       model.Add("categories", cats);
-//       // Item logic
-//       string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-//       ApplicationUser currentUser = await _userManager.FindByIdAsync(userId); //find the current user
-//       if (currentUser != null) //if there is a current user, show the items in the categories
-//       {
-//         Item[] items = _db.Items
-//                     .Where(entry => entry.User.Id == currentUser.Id)
-//                     .ToArray();
-//         model.Add("items", items);
-//       }
-//       return View(model);
-//     }
-//   }
-// }
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      Treat[] treats = _db.Treats.ToArray();
+      Flavor[] flavors = _db.Flavors.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("treats", treats);
+      model.Add("flavors", flavors);
+      return View(model);
+    }
+  }
+}
--- PierreIdentity/Models/Treat.cs
+++ PierreIdentity/Models/Treat.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-
 namespace PierreIdentity.Models
 {
   public class Treat
