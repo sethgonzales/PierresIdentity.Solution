57a1e7a3e7cee91fd6ef23bc42f9205414e91457
sethgonzales <sethgonzales157@gmail.com>
2023-10-20 08:21:55 -0700

    initialize file structures and gitignore

---
+++ PierreIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PierreIdentity.Solution.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
