a5883fd10a7c75642af3062b75716dbd47029db8
sethgonzales <sethgonzales157@gmail.com>
2023-10-20 09:52:31 -0700

    add treat index, edit, and details views

--- PierreIdentity/Models/Flavor.cs
+++ PierreIdentity/Models/Flavor.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PierreIdentity.Models
 {
   public class Flavor
   {
     public int FlavorId { get; set; }
+    [Required(ErrorMessage = "The flavor's name can't be empty!")]
     public string Name { get; set; }
     public List<TreatFlavor> JoinEntities { get; }
   }
